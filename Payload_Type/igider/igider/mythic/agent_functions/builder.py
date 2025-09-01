d3e824ef7a84b0c1bbdbb96f6c857b1bb18cc508
MohamedHdidane <mohamed642hdidane@gmail.com>
2025-09-01 15:49:13 +0100

    fix_container

--- Payload_Type/igider/igider/mythic/agent_functions/builder.py
+++ Payload_Type/igider/igider/mythic/agent_functions/builder.py
@@ -29,7 +29,7 @@ class Igider(PayloadType):
     wrapper = False
     wrapped_payloads = ["pickle_wrapper"]
     mythic_encrypts = False
-    translation_container = "binarytranslator"
+    translation_container = "translator"
     note = "Production-ready Python agent with advanced obfuscation and encryption features"
     supports_dynamic_loading = True
     
